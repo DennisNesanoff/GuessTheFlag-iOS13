c495b2c595a7faae7eb72e10978824834fe07722
Dennis <nndenninn@gmail.com>
2020-03-07 19:34:04 +0300

    Initial Commit

---
+++ GuessTheFlag-iOS13/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GuessTheFlag-iOS13
+//
+//  Created by Dennis Nesanoff on 07.03.2020.
+//  Copyright © 2020 Dennis Nesanoff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
