28d4f26e7cbb0f510c12ff6da0a8c83a4d42c25e
Dennis <nndenninn@gmail.com>
2020-03-07 21:17:00 +0300

    Added IBAction

--- GuessTheFlag-iOS13/ViewController.swift
+++ GuessTheFlag-iOS13/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet var button3: UIButton!
     
     var countries = [String]()
+    var correctAnswer = 0
     var score = 0
     
     override func viewDidLoad() {
@@ -31,12 +32,44 @@ class ViewController: UIViewController {
         
         askQuestion()
     }
-
+    
     func askQuestion() {
         countries.shuffle()
+        
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        correctAnswer = Int.random(in: 0...2)
+        title = countries[correctAnswer].uppercased()
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        switch sender.tag {
+        case 0:
+            if correctAnswer == 0 {
+                print("")
+                askQuestion()
+            } else {
+                askQuestion()
+            }
+        case 1:
+            if correctAnswer == 1 {
+                print("")
+                askQuestion()
+            } else {
+                askQuestion()
+            }
+        case 2:
+            if correctAnswer == 2 {
+                print("")
+                askQuestion()
+            } else {
+                askQuestion()
+            }
+        default:
+            return
+        }
     }
 }
 
